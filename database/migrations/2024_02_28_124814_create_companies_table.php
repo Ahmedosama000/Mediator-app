63c69f70c7d837c7e19e5f48edbe03928b8b8a3a
Ahmed Osama <Trezegea@gmail.com>
2024-02-29 15:06:26 +0200

    Finish Migration

--- database/migrations/2024_02_28_124814_create_companies_table.php
+++ database/migrations/2024_02_28_124814_create_companies_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name',64);
+            $table->string('device_name',30);
             $table->string('email',64)->unique();
             $table->string('password',64);
             $table->timestamp('email_verified_at')->nullable();
